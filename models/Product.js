6e5675c86eaa7fe4b5322a1e6f4b9b82b1f8ffdb
Jonny Gemme <gemmejonathan@gmail.com>
2022-06-04 17:31:06 -0400

    Changed File Structure

--- models/Product.js
+++ models/Product.js
@@ -23,7 +23,24 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      
+      validate: {
+        isDecimal: true,
+    }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      }
     }
   },
   {
